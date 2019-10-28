53978f46a2b4fa384133782c5f896b1097f52092
Panji Y. Wiwaha <panjiyudasetya@gmail.com>
2019-10-28 11:00:33 +0700

    Updated requirements and setup files

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setuptools.setup(
     version="0.0.1",
     author="Panji Y. Wiwaha",
     author_email="panjiyudasetya@gmail.com",
-    description="A Django app to generate Twilio access token for particular services.",
+    description="A Django app to generate Twilio access token for particular Twilio services.",
     keywords=['django', 'twilio', 'access', 'token'],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/senseobservationsystems/django-twilio-access-token/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Environment :: Web Environment",
@@ -20,10 +20,18 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     install_requires=[
         'django',
         'djangorestframework',
-        'twilio'],
-    python_requires='>=3.6',
+        'python-dateutil',
+        'twilio'
+    ]
 )
