a8cd39ecdfd98fd4dce80f2541e24e42db865eb0
Panji Y. Wiwaha <panjiyudasetya@gmail.com>
2019-10-31 18:33:47 +0700

    Reflected feedback from Ricky : Part 1 - Remove token wrapper

--- django_twilio_access_token/settings.py
+++ django_twilio_access_token/settings.py
@@ -9,8 +9,15 @@ Discover all Twilio secrets from the project settings.
 from .utils import discover_twilio_account_sid, discover_twilio_video_credentials
 
 
-# Twilio Account
+# TWILIO_ACCOUNT_SID:
+#   A key that you can find it on the Twilio console dashboard.
+#   https://www.twilio.com/console/dashboard/
 TWILIO_ACCOUNT_SID = discover_twilio_account_sid()
 
-# Twilio Programmable Video Key
+# Twilio : Programmable Video
+# TWILIO_VIDEO_API_KEY_SID:
+#    A key that you need to generate it through:
+#    https://www.twilio.com/console/video/project/api-keys/
+# TWILIO_VIDEO_API_KEY_SECRET:
+#    A secret key that you can obtain altogether when you create TWILIO_VIDEO_API_KEY_SID.
 TWILIO_VIDEO_API_KEY_SID, TWILIO_VIDEO_API_KEY_SECRET = discover_twilio_video_credentials()
--- django_twilio_access_token/views.py
+++ django_twilio_access_token/views.py
@@ -1,32 +1,41 @@
-from django.core.exceptions import ImproperlyConfigured, ValidationError
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VideoGrant
 
-from .tokens import TwilioAccessToken
 from .serializers import VideoTokenDeserializer
+from .settings import TWILIO_ACCOUNT_SID, \
+    TWILIO_VIDEO_API_KEY_SID, TWILIO_VIDEO_API_KEY_SECRET
 
 
 class TwilioAccessTokenViewSet(viewsets.ViewSet):
 
     @action(methods=['post'], detail=False)
     def video(self, request):
-        serializer = VideoTokenDeserializer(data=request.data)
+        """
+        Get token for video calling over particular room.
+        """
 
-        if not serializer.is_valid():
+        # validate request.data from incoming request.
+        try:
+            serializer = VideoTokenDeserializer(data=request.data)
+            serializer.is_valid(True)
+        except ValidationError:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        validated_data = serializer.data
-        twilio = TwilioAccessToken(validated_data['identity'], validated_data['valid_until'])
+        validated_data = serializer.validated_data
 
-        try:
-            twilio.grant_video_call_permission(validated_data['room_name'])
-        except ValidationError as e:
-            return Response(e.message_dict, status=status.HTTP_400_BAD_REQUEST)
+        # create access token for Twilio Video
+        twilio_token = AccessToken(
+            account_sid=TWILIO_ACCOUNT_SID, signing_key_sid=TWILIO_VIDEO_API_KEY_SID,
+            secret=TWILIO_VIDEO_API_KEY_SECRET, valid_until=validated_data['valid_until'])
+        twilio_token.identity = validated_data['identity']
 
-        try:
-            return Response(twilio.get_token(), status=status.HTTP_201_CREATED)
-        except ValidationError as e:
-            return Response(e.message_dict, status=status.HTTP_400_BAD_REQUEST)
-        except ImproperlyConfigured as e:
-            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # create video grant instance
+        video_grant = VideoGrant(room=validated_data['room_name'])
+        twilio_token.add_grant(video_grant)
+
+        token = {'token': twilio_token.to_jwt()}
+        return Response(token, status=status.HTTP_201_CREATED)
