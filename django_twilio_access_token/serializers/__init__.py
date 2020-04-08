7625901645876e364c60e23f6197c48814c280da
Panji Y. Wiwaha <panjiyudasetya@gmail.com>
2020-04-08 12:16:54 +0700

    Addressed feedback : separation concern between serializing and deserializing

--- django_twilio_access_token/serializers/__init__.py
+++ django_twilio_access_token/serializers/__init__.py
@@ -1,8 +1,9 @@
 from .twilio_access_token_serializer import VideoTokenDeserializer
-from .twilio_room_serializer import GroupRoomDeserializer, PeerToPeerRoomDeserializer
+from .twilio_room_serializer import GroupRoomDeserializer, PeerToPeerRoomDeserializer, RoomSerializer
 
 __all__ = [
     'VideoTokenDeserializer',
     'GroupRoomDeserializer',
-    'PeerToPeerRoomDeserializer'
+    'PeerToPeerRoomDeserializer',
+    'RoomSerializer'
 ]
--- django_twilio_access_token/serializers/twilio_room_serializer.py
+++ django_twilio_access_token/serializers/twilio_room_serializer.py
@@ -1,4 +1,18 @@
+from django.conf import settings
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from twilio.base.exceptions import TwilioException, TwilioRestException
+from twilio.rest import Client
+
+
+class RoomSerializer(serializers.Serializer):
+    def to_representation(self, instance):
+        """
+        Convert room instance into expected response.
+
+        :param twilio.rest.video.v1.room.RoomInstance instance: Twilio Room instance.
+        """
+        return {'room_name': instance.unique_name, 'sid': instance.sid}
 
 
 class BaseRoomDeserializer(serializers.Serializer):
@@ -7,7 +21,47 @@ class BaseRoomDeserializer(serializers.Serializer):
     of the particular Twilio room serializer.
     """
     status_callback = serializers.URLField(default=None)
-    unique_name = serializers.CharField(min_length=5)
+    room_name = serializers.CharField(min_length=34, max_length=34)
+
+    def create(self, validated_data):
+        """
+        Create Twilio room instance.
+
+        :param dict validated_data: Validated data.
+        :returns: Twilio room instance
+        :rtype: twilio.rest.video.v1.room.RoomInstance
+        """
+        try:
+            client = Client(account_sid=settings.TWILIO_ACCOUNT_SID,
+                            username=settings.TWILIO_ACCOUNT_SID,
+                            password=settings.TWILIO_AUTH_TOKEN)
+        except TwilioException as e:
+            # `TwilioException` arised when username and password is not provided
+            detail = self._get_twilio_error_dict(str(e), 'invalid-settings')
+            raise ValidationError(detail=detail, code='invalid')
+
+        try:
+            room_name = validated_data.pop('room_name')
+            room = client.video.rooms.create(**validated_data, unique_name=room_name)
+        except TwilioRestException as e:
+            # `TwilioRestException` arised when the Twilio Client attempt to create a Room
+            detail = self._get_twilio_error_dict(e.msg, e.code)
+            raise ValidationError(detail=detail, code='invalid')
+
+        return room
+
+    def _get_twilio_error_dict(self, msg, code=None):
+        """
+        Convert incoming message and code into Twilio Error dictionary.
+
+        :param str msg: A human-readable message for the Twilio error.
+        :param int|None code: A Twilio-specific error code for the error. This is
+            not available for all errors.
+        :returns: Twilio Error dictionary
+        :rtype: dict
+        """
+        err_code = 'unknown' if code is None else code
+        return {'twilio_err_code': err_code, 'twilio_err_msg': msg}
 
 
 class GroupRoomDeserializer(BaseRoomDeserializer):
