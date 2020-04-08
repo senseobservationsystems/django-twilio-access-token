cde6558c9d72454e488f8d837b564e51942ce044
Panji Y. Wiwaha <panjiyudasetya@gmail.com>
2020-04-08 12:18:21 +0700

    Adjusted deserializers tests

--- test_project/test_app/tests/test_serializers/test_room_deserializers.py
+++ test_project/test_app/tests/test_serializers/test_room_deserializers.py
@@ -1,7 +1,20 @@
 from django.test import TestCase
 from rest_framework.exceptions import ErrorDetail, ValidationError
+from twilio.rest.video.v1.room import RoomInstance
 
-from django_twilio_access_token.serializers import GroupRoomDeserializer, PeerToPeerRoomDeserializer
+from django_twilio_access_token.serializers import GroupRoomDeserializer, PeerToPeerRoomDeserializer, RoomSerializer
+
+
+class TestRoomSerializer(TestCase):
+
+    def test_serializer(self):
+        """Test response serializer must be exactly like we expect"""
+        instance = RoomInstance(
+            version='test',
+            payload={'unique_name': '1234567890123456789012345678901234', 'sid': 'random-session-id'})
+
+        expected_response = {'room_name': '1234567890123456789012345678901234', 'sid': 'random-session-id'}
+        self.assertDictEqual(RoomSerializer(instance).data, expected_response)
 
 
 class TestGroupRoomDeserializer(TestCase):
@@ -9,7 +22,7 @@ class TestGroupRoomDeserializer(TestCase):
     def setUp(self):
         self.payload = {
             "status_callback": "http://example.org",
-            "unique_name": "some-unique-name",
+            "room_name": "1234567890123456789012345678901234",
             "record_participants_on_connect": True,
             "type": "group"
         }
@@ -38,9 +51,9 @@ class TestGroupRoomDeserializer(TestCase):
         self.assertIsNotNone(deserializer.validated_data)
         self.assertDictEqual(deserializer.validated_data, self.payload)
 
-    def test_deserializer_with_unique_name_less_than_five_chars(self):
-        """Test group room deserializer with `unique_name` less than 5 characters is not allowed"""
-        self.payload['unique_name'] = "abcd"
+    def test_deserializer_with_room_name_less_than_minimum_chars(self):
+        """Test group room deserializer with `room_name` less than 34 characters is not allowed"""
+        self.payload['room_name'] = "abcd"
 
         deserializer = GroupRoomDeserializer(data=self.payload)
         with self.assertRaises(ValidationError) as ctx:
@@ -48,12 +61,25 @@ class TestGroupRoomDeserializer(TestCase):
 
         self.assertDictEqual(
             ctx.exception.detail,
-            {'unique_name': [ErrorDetail(string='Ensure this field has at least 5 characters.', code='min_length')]}
+            {'room_name': [ErrorDetail(string='Ensure this field has at least 34 characters.', code='min_length')]}
+        )
+
+    def test_deserializer_with_room_name_more_than_maximum_chars(self):
+        """Test group room deserializer with `room_name` more than 34 characters is not allowed"""
+        self.payload['room_name'] = "12345678901234567890123456789012345"
+
+        deserializer = GroupRoomDeserializer(data=self.payload)
+        with self.assertRaises(ValidationError) as ctx:
+            deserializer.is_valid(raise_exception=True)
+
+        self.assertDictEqual(
+            ctx.exception.detail,
+            {'room_name': [ErrorDetail(string='Ensure this field has no more than 34 characters.', code='max_length')]}
         )
 
     def test_deserializer_without_unique_name(self):
-        """Test group room deserializer without `unique_name` is not allowed"""
-        del self.payload['unique_name']
+        """Test group room deserializer without `room_name` is not allowed"""
+        del self.payload['room_name']
 
         deserializer = GroupRoomDeserializer(data=self.payload)
         with self.assertRaises(ValidationError) as ctx:
@@ -61,7 +87,7 @@ class TestGroupRoomDeserializer(TestCase):
 
         self.assertDictEqual(
             ctx.exception.detail,
-            {'unique_name': [ErrorDetail(string='This field is required.', code='required')]}
+            {'room_name': [ErrorDetail(string='This field is required.', code='required')]}
         )
 
     def test_deserializer_without_status_callback(self):
@@ -122,13 +148,13 @@ class TestPeerToPeerRoomDeserializer(TestCase):
     def setUp(self):
         self.payload = {
             "status_callback": "http://example.org",
-            "unique_name": "some-unique-name",
+            "room_name": "1234567890123456789012345678901234",
             "enable_turn": True,
             "type": "peer-to-peer"
         }
 
     def test_deserializer_peer_to_peer_with_valid_payload(self):
-        """Test group room deserializer with valid payload"""
+        """Test peer-to-peer room deserializer with valid payload"""
         deserializer = PeerToPeerRoomDeserializer(data=self.payload)
         try:
             deserializer.is_valid(raise_exception=True)
@@ -138,9 +164,9 @@ class TestPeerToPeerRoomDeserializer(TestCase):
         self.assertIsNotNone(deserializer.validated_data)
         self.assertDictEqual(deserializer.validated_data, self.payload)
 
-    def test_deserializer_with_unique_name_less_than_five_chars(self):
-        """Test group room deserializer with `unique_name` less than 5 characters is not allowed"""
-        self.payload['unique_name'] = "abcd"
+    def test_deserializer_with_room_name_less_than_minimum_chars(self):
+        """Test peer-to-peer room deserializer with `room_name` less than 34 characters is not allowed"""
+        self.payload['room_name'] = "abcd"
 
         deserializer = PeerToPeerRoomDeserializer(data=self.payload)
         with self.assertRaises(ValidationError) as ctx:
@@ -148,12 +174,25 @@ class TestPeerToPeerRoomDeserializer(TestCase):
 
         self.assertDictEqual(
             ctx.exception.detail,
-            {'unique_name': [ErrorDetail(string='Ensure this field has at least 5 characters.', code='min_length')]}
+            {'room_name': [ErrorDetail(string='Ensure this field has at least 34 characters.', code='min_length')]}
+        )
+
+    def test_deserializer_with_room_name_more_than_maximum_chars(self):
+        """Test peer-to-peer room deserializer with `room_name` more than 34 characters is not allowed"""
+        self.payload['room_name'] = "12345678901234567890123456789012345"
+
+        deserializer = PeerToPeerRoomDeserializer(data=self.payload)
+        with self.assertRaises(ValidationError) as ctx:
+            deserializer.is_valid(raise_exception=True)
+
+        self.assertDictEqual(
+            ctx.exception.detail,
+            {'room_name': [ErrorDetail(string='Ensure this field has no more than 34 characters.', code='max_length')]}
         )
 
     def test_deserializer_without_unique_name(self):
-        """Test group room deserializer without `unique_name` is not allowed"""
-        del self.payload['unique_name']
+        """Test peer-to-peer room deserializer without `room_name` is not allowed"""
+        del self.payload['room_name']
 
         deserializer = PeerToPeerRoomDeserializer(data=self.payload)
         with self.assertRaises(ValidationError) as ctx:
@@ -161,11 +200,11 @@ class TestPeerToPeerRoomDeserializer(TestCase):
 
         self.assertDictEqual(
             ctx.exception.detail,
-            {'unique_name': [ErrorDetail(string='This field is required.', code='required')]}
+            {'room_name': [ErrorDetail(string='This field is required.', code='required')]}
         )
 
     def test_deserializer_without_status_callback(self):
-        """Test group room deserializer without `status_callback` is allowed"""
+        """Test peer-to-peer room deserializer without `status_callback` is allowed"""
         del self.payload['status_callback']
 
         deserializer = PeerToPeerRoomDeserializer(data=self.payload)
@@ -178,7 +217,7 @@ class TestPeerToPeerRoomDeserializer(TestCase):
         self.assertDictContainsSubset(self.payload, deserializer.validated_data)
 
     def test_deserializer_without_enable_turn(self):
-        """Test group room deserializer without `enable_turn` is allowed"""
+        """Test peer-to-peer room deserializer without `enable_turn` is allowed"""
         del self.payload['enable_turn']
 
         deserializer = PeerToPeerRoomDeserializer(data=self.payload)
@@ -191,7 +230,7 @@ class TestPeerToPeerRoomDeserializer(TestCase):
         self.assertDictContainsSubset(self.payload, deserializer.validated_data)
 
     def test_deserializer_with_invalid_type(self):
-        """Test group room deserializer without invalid room type is not allowed"""
+        """Test peer-to-peer room deserializer without invalid room type is not allowed"""
         self.payload['type'] = "unknown"
 
         deserializer = PeerToPeerRoomDeserializer(data=self.payload)
@@ -204,7 +243,7 @@ class TestPeerToPeerRoomDeserializer(TestCase):
         )
 
     def test_deserializer_with_invalid_status_callback(self):
-        """Test group room deserializer with invalid status callback is not allowed"""
+        """Test peer-to-peer room deserializer with invalid status callback is not allowed"""
         self.payload['status_callback'] = "invalid-url"
 
         deserializer = PeerToPeerRoomDeserializer(data=self.payload)
