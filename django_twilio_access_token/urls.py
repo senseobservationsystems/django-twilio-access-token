db31e5c75ba77a056f4ca6bf0e2772dcf623c760
Panji Y. Wiwaha <panjiyudasetya@gmail.com>
2020-04-08 12:15:46 +0700

    Addressed feedback : use proper url names

--- django_twilio_access_token/urls.py
+++ django_twilio_access_token/urls.py
@@ -10,6 +10,6 @@ router.register(r'token', views.TwilioAccessTokenViewSet, base_name='twilio-toke
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('room/', views.TwilioPeerToPeerRoomView.as_view(), name='peer-to-peer-room'),
-    path('room/group/', views.TwilioGroupRoomView.as_view(), name='group-room'),
+    path('rooms/peer2peer/', views.TwilioPeerToPeerRoomView.as_view(), name='peer-to-peer-room'),
+    path('rooms/group/', views.TwilioGroupRoomView.as_view(), name='group-room'),
 ]
